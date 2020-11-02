bea559714fb2f40bc31bc9bfbd72853555fc2cd8
Fabrizio <whoet.wo@gmail.com>
2020-11-02 16:43:45 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.header-right a > i').click(function(){
+   $('.hamburger-menu').toggleClass('active')
+})
+
+$('.hamburger-menu a > i').click(function(){
+   $('.hamburger-menu').toggleClass('active')
+})
